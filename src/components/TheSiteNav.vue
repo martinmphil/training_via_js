e6e50db8b35fcc9fbbf5d96af649e1b5b8db1b1b
Martin <martin.mphil@gmail.com>
2020-10-10 10:13:00 +0100

    Homepage link text changed to "Training via JS"

--- src/components/TheSiteNav.vue
+++ src/components/TheSiteNav.vue
@@ -1,8 +1,10 @@
 <template>
   <nav class="nav">
-      <router-link to="/">Home</router-link>
-      <router-link to="/sensor">Sensors</router-link>
-      <router-link to="/functional-programming">Functional-programming</router-link>
+    <router-link to="/">Training via JS</router-link>
+    <router-link to="/sensor">Sensors</router-link>
+    <router-link to="/functional-programming"
+      >Functional-programming</router-link
+    >
   </nav>
 </template>
 
@@ -23,7 +25,8 @@ export default {
   border: solid;
   border-color: rgb(0, 0, 102);
   border-radius: 0.5em;
-  box-shadow: 0 0.5em 0.4em 0 rgba(0,0,102,0.4), 0 0.4em 0.5em 0 rgba(0,0,102,0.3);
+  box-shadow: 0 0.5em 0.4em 0 rgba(0, 0, 102, 0.4),
+    0 0.4em 0.5em 0 rgba(0, 0, 102, 0.3);
   padding: 0.7em;
   margin: 1em auto;
   cursor: pointer;
